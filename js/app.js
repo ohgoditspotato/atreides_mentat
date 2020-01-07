5e71a72b5ed8ebbd7c655d94f883ea55d30560ce
rdt <robtaylor234@gmail.com>
2020-01-07 13:37:17 +0000

    working page

---
+++ js/app.js
@@ -0,0 +1,5 @@
+"use strict";
+function App() {
+    return React.createElement("div", null, "Hello world");
+}
+ReactDOM.render(React.createElement(App, null), document.getElementById("root"));
---
+++ src/app.tsx
@@ -0,0 +1,5 @@
+function App(): JSX.Element {
+  return <div>Hello world</div>;
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
