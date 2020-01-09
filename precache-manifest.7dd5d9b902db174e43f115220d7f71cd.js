fd963fc209b2d48ea4cb02d24623c388b7ddabae
Robert Taylor <robtaylor234@gmail.com>
2020-01-09 15:28:33 +0000

    Updates

---
+++ precache-manifest.7dd5d9b902db174e43f115220d7f71cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c234ffd61d11e5a1f7ed3f0a1772511",
+    "url": "/atreides_mentat/index.html"
+  },
+  {
+    "revision": "95ab96d4081f55efd158",
+    "url": "/atreides_mentat/static/js/2.529f9893.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/atreides_mentat/static/js/2.529f9893.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a3cb85910f34070b56bc",
+    "url": "/atreides_mentat/static/js/main.fa3424d7.chunk.js"
+  },
+  {
+    "revision": "31199c8e39e863c08e1d",
+    "url": "/atreides_mentat/static/js/runtime-main.d88582d5.js"
+  }
+]);
