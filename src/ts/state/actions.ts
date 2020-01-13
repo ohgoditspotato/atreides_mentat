0f0d5f4690179116f067b79ce3ab561e93466d3d
Robert Taylor <robtaylor234@gmail.com>
2020-01-13 17:08:08 +0000

    add format command

--- src/ts/state/actions.ts
+++ src/ts/state/actions.ts
@@ -9,23 +9,29 @@ export const show_view_cards_modal = createAction<house_name_t>("view/view_cards
 export const show_add_cards_modal = createAction<house_name_t>("view/add_card");
 export const show_reset_game_modal = createAction("view/reset");
 
-export const house_add_card = createAction("houses/add_card", (house: house_name_t, card: treachery_card_t) => {
-  return {
-    payload: {
-      house,
-      card,
-    },
-  };
-});
+export const house_add_card = createAction(
+  "houses/add_card",
+  (house: house_name_t, card: treachery_card_t) => {
+    return {
+      payload: {
+        house,
+        card,
+      },
+    };
+  }
+);
 
-export const house_remove_card = createAction("houses/remove_card", (house: house_name_t, index: number) => {
-  return {
-    payload: {
-      house,
-      index,
-    },
-  };
-});
+export const house_remove_card = createAction(
+  "houses/remove_card",
+  (house: house_name_t, index: number) => {
+    return {
+      payload: {
+        house,
+        index,
+      },
+    };
+  }
+);
 
 export const house_modify_spice = createAction(
   "houses/modify_spice",
