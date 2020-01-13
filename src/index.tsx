ee2c0db7c7a5e2eba5d13ed0e937e96978503062
Robert Taylor <robtaylor234@gmail.com>
2020-01-13 17:29:07 +0000

    revert previous - just replace icons from create-react-app

--- src/index.tsx
+++ src/index.tsx
@@ -17,6 +17,16 @@ ReactDOM.render(
       </div>
     </section>
     <GameContainer />
+    <footer className="footer">
+      Icons made by{" "}
+      <a href="https://www.flaticon.com/authors/freepik" title="Freepik">
+        Freepik
+      </a>{" "}
+      from{" "}
+      <a href="https://www.flaticon.com/" title="Flaticon">
+        www.flaticon.com
+      </a>
+    </footer>
   </Provider>,
   document.getElementById("root")
 );
