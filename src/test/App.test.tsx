4507e057114cf8702be0b37142774e8a49c4048f
Robert Taylor <robtaylor234@gmail.com>
2020-01-14 13:22:50 +0000

    remove tests, not in use

--- src/test/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import GameContainer from "ts/components/GameContainer";
-
-test("renders learn react link", () => {
-  const { getByText } = render(<GameContainer />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
